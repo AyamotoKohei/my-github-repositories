2c5e9fd606059f3a16dd544be1ffb09a19e6a675
AyamotoKohei <koh14taka2@gmail.com>
2021-12-13 23:39:26 +0900

    add: GitHubリポジトリの表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
-import useSWR from "swr";
 import Head from "next/head";
-import { Octokit } from "@octokit/rest";
 
-export const Home = () => {
+export const Home = ({ repos }) => {
   return (
     <div>
       <Head>
         <title>Home Page</title>
       </Head>
+      <ul>
+        {repos.map((repo) => {
+          return <li key={repo.id}>{repo.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const response = await fetch(
+    `https://api.github.com/users/AyamotoKohei/repos`
+  );
+  const repos = await response.json();
+  return { props: { repos } };
+}
 
 export default Home;
